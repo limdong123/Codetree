9a2f9b7b8da4c2fc78d843a64b1aa2aa0ef38965
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-04-29 00:41:21 +0900

    Title: 약수가 세 개인 수, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250429/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,14 @@
+start, end = map(int, input().split())
+
+# Please write your code here.
+total = 0
+for i in range(start, end+1):
+    cnt = 0
+    for j in range(1, i+1):
+        if(i%j == 0):
+            cnt += 1
+    
+    if(cnt == 3):
+        total +=1
+
+print(total)
