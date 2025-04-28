cab4d36bedd10b03620a00bb7faf0a7926ff6f30
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-04-28 23:16:14 +0900

    Title: a로 채워넣기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250428/a로 채워넣기/filling-with-a.py
+++ 250428/a로 채워넣기/filling-with-a.py
@@ -1,5 +1,5 @@
 st = input()
 
-st = st.replace(st[1], "a",1)
-st = st.replace(st[-2], "a",1)
-print(st)
+new_st1 = st.replace(st[1], "a",1)
+new_st2 = new_st1.replace(st[-2], "a",1)
+print(new_st2)
