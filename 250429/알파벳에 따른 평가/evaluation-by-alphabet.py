f4b80d345b1a2e67904c63c3c3306cfa36ec46e1
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-04-29 23:31:57 +0900

    Title: 알파벳에 따른 평가, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250429/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+n = input()
+
+if(n == 'S'):
+    print("Superior")
+elif(n == 'A'):
+    print("Excellent")
+elif(n == 'B'):
+    print("Good")
+elif(n == 'C'):
+    print("Usually")
+elif(n == 'E'):
+    print("Effort")
+else:
+    print("Failure")
