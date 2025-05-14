6afd97ec7d6118cf3bc4f87646536b69960d116c
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-14 16:36:19 +0900

    Title: 오름 내림차순 정렬, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250514/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,13 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+# Please write your code here.
+
+nums.sort()
+for i in nums:
+    print(i, end=" ")
+print()
+nums.sort(reverse = True)
+for i in nums:
+    print(i, end = " ")
+print()
