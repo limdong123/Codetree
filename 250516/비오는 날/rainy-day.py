c12a400fa26aadeb44fe64d1041166f816aed6ed
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-16 15:52:26 +0900

    Title: 비오는 날, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250516/비오는 날/rainy-day.py
@@ -0,0 +1,13 @@
+n = int(input())
+date = []
+day = []
+weather = []
+
+for _ in range(n):
+    d, dy, w = input().split()
+    date.append(d)
+    day.append(dy)
+    weather.append(w)
+
+# Please write your code here.
+print(date)
