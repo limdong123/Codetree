95843f76be67852941431db4322e4a40fb28fe67
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-16 16:02:26 +0900

    Title: 비오는 날, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250516/비오는 날/rainy-day.py
+++ 250516/비오는 날/rainy-day.py
@@ -10,4 +10,23 @@ for _ in range(n):
     weather.append(w)
 
 # Please write your code here.
-print(date)
+class Weather:
+    def __init__(self, date = "", day = "", weather = ""):
+        self.date = date
+        self.day = day
+        self.weather = weather
+
+weathers = []
+result_idx = 0
+for i  in range(n):
+    weathers.append(Weather(date[i], day[i], weather[i]))
+    if weathers[i].weather == "Rain":
+        result_idx = i
+
+
+for i in range(result_idx - 1 , -1, -1):# weather이 rain이고 가장 작은 date 구하기
+    if weathers[i].weather == "Rain" and weathers[i].date < weathers[result_idx].date:
+        result_idx = i
+
+print(weathers[result_idx].date, weathers[result_idx].day, weathers[result_idx].weather)
+
