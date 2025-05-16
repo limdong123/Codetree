94000da7d4b1a4fe37c1b4b0ea511c14ede24fbe
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-16 15:38:29 +0900

    Title: 사는 지역, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250516/사는 지역/where-live.py
@@ -0,0 +1,29 @@
+n = int(input())
+name = []
+street_address = []
+region = []
+
+for _ in range(n):
+    n_i, s_i, r_i = input().split()
+    name.append(n_i)
+    street_address.append(s_i)
+    region.append(r_i)
+
+# Please write your code here.
+#이름이 가장 느린사람 자료 출력
+class User:
+    def __init__(self, name = "", street_address = "", region = ""):
+        self.name = name
+        self.street_address = street_address
+        self.region = region
+
+user = []
+for i in range(n): #클래스에 값 입력
+    user.append(User(name[i], street_address[i], region[i]))
+
+result_idx = 0
+for i in range(1, n): # 클래스 이름 가장 큰 사람 찾기
+    if user[i].name < user[i-1].name:
+        result_idx = i
+
+print("aim" >= "part")
