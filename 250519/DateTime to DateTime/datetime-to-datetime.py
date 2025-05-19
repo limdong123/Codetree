4331fac5bc3900e2dadf44964482c7ae12568c59
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-19 19:07:20 +0900

    Title: DateTime to DateTime, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250519/DateTime to DateTime/datetime-to-datetime.py
+++ 250519/DateTime to DateTime/datetime-to-datetime.py
@@ -5,4 +5,8 @@ a, b, c = map(int, input().split())
 # 시 = b*60
 # 일 = a * 60 * 24
 
-print((a*60*24 + b*60 + c) -(11*60*24 + 11*60 + 11))
+result = (a*60*24 + b*60 + c) -(11*60*24 + 11*60 + 11)
+if result < 0:
+    print(-1)
+else:
+    print(result)
