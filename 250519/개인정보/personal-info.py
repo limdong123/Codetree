4bf2c93ad724d7cde78b9c8585ffc5aae884520e
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-19 16:47:25 +0900

    Title: 개인정보, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250519/개인정보/personal-info.py
@@ -0,0 +1,31 @@
+n = 5
+name = []
+height = []
+weight = []
+
+for _ in range(n):
+    n, h, w = input().split()
+    name.append(n)
+    height.append(int(h))
+    weight.append(float(w))
+
+# Please write your code here.
+class Data:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.h = height
+        self.w = weight
+
+datas = []
+for i in range(5):
+    datas.append(Data(name[i], height[i], weight[i]))
+
+datas.sort(key = lambda x : x.name[0])
+print("name")
+for i in range(5):
+    print(f"{datas[i].name} {datas[i].h} {datas[i].w}")
+print()
+datas.sort(key = lambda x : -x.h)
+print("height")
+for i in range(5):
+    print(f"{datas[i].name} {datas[i].h} {datas[i].w}")
