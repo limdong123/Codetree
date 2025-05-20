ca8dae84aba6f1003ea689c0ad8ec8b2dd97fffd
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-20 16:46:03 +0900

    Title: 그 요일은, Time: 55ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250520/그 요일은/the-day-of-the-day.py
+++ 250520/그 요일은/the-day-of-the-day.py
@@ -4,7 +4,7 @@ A = input()
 # Please write your code here.
 #          1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+days = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 def month_is(m, d):
     sum = 0
@@ -15,10 +15,14 @@ def month_is(m, d):
 a_day = month_is(m1, d1)
 b_day = month_is(m2, d2)
 
-result_day = b_day - a_day
+result_day = b_day - a_day + 1
+
+result = result_day//7
+result_day %= 7
+
 for i in range(7):
     if days[i] == A:
-        result_day - i 
-        break
+        if result_day >= i:
+            result += 1
 
-print(result_day%7)
+print(result)
