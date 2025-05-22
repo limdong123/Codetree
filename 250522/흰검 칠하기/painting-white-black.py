df4578e7a714848ae79a7acb957ebc5061c0373a
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-22 16:14:11 +0900

    Title: 흰검 칠하기, Time: 48ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250522/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,11 @@
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+# Please write your code here.
+offset = 1000*100
+points = [""] * offset * 2
