1e3bdf8737c9ea14fd32fd0a36dda1ca80fd9790
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-22 19:59:04 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 98ms, Memory: 47MB, Status: Wrong Answer - Codetree

---
+++ 250522/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,31 @@
+x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
+x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
+x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+
+# Please write your code here.
+# 처음[0] 직사각형 넓이 배열에 1로 바꾸기 => 두번째 직사각형 배열 0으로 바꾸기 
+# => 이후 1이 나오는 값 x 의 최솟값 최댓값, y 최솟값, 최댓값으로 직사각형 최소 넓이 구하기
+OFFSET = 1000
+size = OFFSET * 2
+maps = [[0] * size for _ in range(size)]
+
+for i in range(x1[0] + OFFSET, x2[0] + OFFSET):
+    for j in range(y1[0] + OFFSET, y2[0] + OFFSET):
+        maps[i][j] = 1
+
+for i in range(x1[1] + OFFSET, x2[1] + OFFSET):
+    for j in range(y1[1] + OFFSET, y2[1] + OFFSET):
+        maps[i][j] = 0
+
+
+min_x, max_x, min_y, max_y = 1001, 0, 1001, 0
+
+for i in range(len(maps)):
+    for j in range(len(maps[i])):
+        if maps[i][j] == 1:
+            min_x = min(min_x, i)
+            max_x = max(max_x, i)
+            min_y = min(min_y, j)
+            max_y = max(max_y, j)
+
+print((max_x - min_x) * (max_y - min_y))
