50458df13b98f8d5dc4284abaa913873324c7d77
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-22 19:47:47 +0900

    Title: 색종이의 총 넓이, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250522/색종이의 총 넓이/the-total-area-of-colored-paper.py
@@ -0,0 +1,22 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x, y = zip(*points)
+x, y = list(x), list(y)
+
+# Please write your code here.
+#x ~ x+8 
+OFFSET = 100
+maps = [[0]*OFFSET*2 for _ in range(OFFSET*2)]
+
+for i in range(n):
+    for j in range(x[i], x[i] + 8):
+        for k in range(y[i], y[i] + 8):
+            maps[j][k] = 1
+
+result = 0
+for row in maps:
+    for cell in row:
+        if cell == 1:
+            result += 1
+
+print(result)
