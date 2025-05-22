1df954dbe7ea7045de4ca55fca266c82aaac7f23
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-22 18:08:43 +0900

    Title: 사각형의 총 넓이 2, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 250522/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 250522/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -9,5 +9,18 @@ for _ in range(n):
 
 # Please write your code here.
 OFFSET =  100
-area = [(0,0) for _ in range(OFFSET)]
-print(area)
+area = [[0] * OFFSET * 2 for _ in range(OFFSET * 2)]
+
+for i in range(n):
+    for j in range(x1[i] + OFFSET, x2[i] + OFFSET):
+        for k in range(y1[i]+OFFSET, y2[i]+OFFSET):
+            area[k][j] += 1
+
+result = 0
+
+for row in area:
+    for cell in row:
+        if cell >= 1:
+            result += 1
+
+print(result)
