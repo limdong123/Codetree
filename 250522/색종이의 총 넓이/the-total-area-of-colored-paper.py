6ea760ded2ecc35386ef60603255c09b2a0c3d56
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-05-22 19:49:41 +0900

    Title: 색종이의 총 넓이, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 250522/색종이의 총 넓이/the-total-area-of-colored-paper.py
+++ 250522/색종이의 총 넓이/the-total-area-of-colored-paper.py
@@ -9,8 +9,8 @@ OFFSET = 100
 maps = [[0]*OFFSET*2 for _ in range(OFFSET*2)]
 
 for i in range(n):
-    for j in range(x[i], x[i] + 8):
-        for k in range(y[i], y[i] + 8):
+    for j in range(x[i] + OFFSET, x[i] + 8+ OFFSET):
+        for k in range(y[i]+ OFFSET, y[i] + 8+ OFFSET):
             maps[j][k] = 1
 
 result = 0
