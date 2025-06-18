081461feb18dc6fb22be3b8db33318a9bf58a6a8
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-06-18 20:32:08 +0900

    Title: 이상한 진수 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250618/이상한 진수 2/awkward-digits-2.py
+++ 250618/이상한 진수 2/awkward-digits-2.py
@@ -1,18 +1,30 @@
 a = input()
 
 # Please write your code here.
+
 a = list(map(int, a))
+
+max_num = 0
 
 for i in range(len(a)):
     if a[i] == 0:
         a[i] = 1
-        break
+    else:
+        a[i] = 0
 
-result = 0
-for i in range(len(a)):
-    result += 2**(len(a) - i - 1) * a[i]
+    # 바꾸고 십진법 전환
+    sum = 0
+    for j in range(len(a)):
+        sum += 2**(len(a) - 1 - j) * a[j]
+    
+    #최댓값 구하기
+    max_num = max(max_num, sum)
+    
+    #초기화
+    if a[i] == 0:
+        a[i] = 1
+    else:
+        a[i] = 0
 
-if(len(a) == 1):
-    result = 0
-print(result)
-
+print(max_num)
+    
