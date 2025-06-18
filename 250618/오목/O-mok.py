24c772bca67b04c6785820c6233c76347186db72
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-06-18 22:47:27 +0900

    Title: 오목, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250618/오목/O-mok.py
@@ -0,0 +1,51 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+
+# Please write your code here.
+#가로 세로 대각선 확인
+
+result = 0
+pointx, pointy = 0, 0
+
+for i in range(19 - 5):
+    for j in range(19 - 5):
+        if board[i][j] != 0:
+            rock = board[i][j]
+            #가로 세로 대각선 확인
+
+            ROW, CELL, DIAG = False, False, False
+            
+            for k in range(1, 5):
+                if rock == board[i][j+k]:
+                    ROW = True
+                else:
+                    ROW = False
+                
+                if rock == board[i+k][j]:
+                    CELL = True
+                else : 
+                    CELL = False
+
+                if rock == board[i+k][j+k]:
+                    DIAG = True
+                else :
+                    DIAG = False
+            
+            if ROW:
+                result = rock
+                pointx = i
+                pointy = j + 2
+            elif CELL:
+                result = rock
+                pointx = i + 2
+                pointy = j
+            elif DIAG:
+                result = rock
+                pointx = i + 2
+                pointy = j + 2
+
+print(result)
+print(pointx + 1, pointy + 1)
+            
+
+
+
