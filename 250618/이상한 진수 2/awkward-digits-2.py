8a03a16d525d9ed9ba90cb62041c5e20a9c91af8
limdong123 <96884089+limdong123@users.noreply.github.com>
2025-06-18 20:15:26 +0900

    Title: 이상한 진수 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250618/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,16 @@
+a = input()
+
+
+# Please write your code here.
+a = list(map(int, a))
+
+for i in range(len(a)):
+    if a[i] == 0:
+        a[i] = 1
+        break
+
+result = 0
+for i in range(len(a)):
+    result += 2**(len(a) - i - 1) * a[i]
+print(result)
+
